9718fa8a810c0dd8faff85d2b0bb8218e87bad1e
Revertron <r@revertron.com>
2021-03-07 17:47:33 +0100

    Tuned some constants adn options.

--- src/blockchain/constants.rs
+++ src/blockchain/constants.rs
@@ -1,6 +1,6 @@
-pub const BLOCK_DIFFICULTY: u32 = 20;
-pub const LOCKER_DIFFICULTY: u32 = 16;
+pub const BLOCK_DIFFICULTY: u32 = 24;
+pub const LOCKER_DIFFICULTY: u32 = 18;
 pub const CHAIN_VERSION: u32 = 1;
-pub const LOCKER_BLOCK_START: u64 = 5;
+pub const LOCKER_BLOCK_START: u64 = 10;
 pub const LOCKER_BLOCK_COUNT: u64 = 3;
 pub const LOCKER_BLOCK_INTERVAL: i64 = 300;
