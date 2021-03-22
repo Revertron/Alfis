4ddacd2f57826e8ca4905e4e606c25596db842df
Revertron <r@revertron.com>
2021-03-22 22:57:54 +0100

    Tuned up difficulty constant for comfortable use with new algo. Mined new origin block to start over test network.

--- src/commons/constants.rs
+++ src/commons/constants.rs
@@ -1,12 +1,12 @@
 pub const CHAIN_VERSION: u32 = 2;
 
-pub const ZONE_DIFFICULTY: u32 = 26;
-pub const BLOCK_DIFFICULTY: u32 = 24;
-pub const LOCKER_DIFFICULTY: u32 = 18;
+pub const ZONE_DIFFICULTY: u32 = 22;
+pub const BLOCK_DIFFICULTY: u32 = 20;
+pub const LOCKER_DIFFICULTY: u32 = 14;
 pub const KEYSTORE_DIFFICULTY: usize = 23;
 
-pub const LOCKER_BLOCK_START: u64 = 12;
-pub const LOCKER_BLOCK_LOCKERS: u64 = 6;
+pub const LOCKER_BLOCK_START: u64 = 16;
+pub const LOCKER_BLOCK_LOCKERS: u64 = 7;
 pub const LOCKER_BLOCK_SIGNS: u64 = 4;
 pub const LOCKER_BLOCK_TIME: i64 = 300;
 pub const LOCKER_BLOCK_INTERVAL: u64 = 50;
