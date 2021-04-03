ed3150cf4aa06f32c3e45ebd3cfbd7b933c3fea3
Revertron <r@revertron.com>
2021-04-03 18:37:44 +0200

    Fixed updating config file on package upgrade.

--- contrib/deb/generate.sh
+++ contrib/deb/generate.sh
@@ -84,8 +84,8 @@ then
   mkdir -p /var/backups
   echo "Backing up configuration file to /var/backups/alfis.conf.`date +%Y%m%d`"
   cp /etc/alfis.conf /var/backups/alfis.conf.`date +%Y%m%d`
-  echo "Normalising and updating /etc/alfis.conf"
-  /usr/bin/alfis -u /var/backups/alfis.conf.`date +%Y%m%d` > /etc/alfis.conf
+  echo "Updating /etc/alfis.conf"
+  /usr/bin/alfis -u /etc/alfis.conf > /etc/alfis.conf
   chgrp alfis /etc/alfis.conf
 
   if command -v systemctl >/dev/null; then
