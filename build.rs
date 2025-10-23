fc7360ea002b5415aebda413a1ae54231a11b2b0
Revertron <r@revertron.com>
2025-10-23 21:43:22 +0200

    Another try to build for macOS.

--- build.rs
+++ build.rs
@@ -1,11 +1,6 @@
 extern crate winres;
 
 fn main() {
-    // Suppress int-conversion warnings for macOS ARM (Apple Silicon) webview compatibility
-    if cfg!(target_os = "macos") && cfg!(target_arch = "aarch64") {
-        println!("cargo:rustc-env=CFLAGS=-Wno-int-conversion");
-    }
-
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("img/logo/alfis.ico");
